45e1e667fc70431f261ac9292e462628afac8c0b
David Brungardt <brungardtdb@gmail.com>
2020-11-18 18:42:48 -0700

    Added some comments and a function to request data via a HTTP request from weatherstack.com to tell the current weather in Aurora Illinois.

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const request = require('request')
 const auth = require('./authentication.js')
 
+// Get authentication key
 const authentication = auth.GetAuthentication()
+// Create URL for weather app request
+// Broken into several strings should we choose to change anything in url
+const website = "http://api.weatherstack.com/"
+const location = "41.763889,-88.290001" // lat and long for Aurora Illinois
+const query = "current?access_key=" + authentication + "&query=" + location
+const url = website + query
 
-
-
-
-
+// Request weather data from weatherstack.com
+request({url: url}, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
--- authentication.js
+++ authentication.js
@@ -1,13 +1,15 @@
 const fs = require('fs')
 
 const GetAuthentication = () => {
+    // Load data from JSON file for authentication
     const dataBuffer = fs.readFileSync('authentication.json')
     const dataJSON = dataBuffer.toString()
     const jsonString =  JSON.parse(dataJSON)  
 
+    // Parse authentication data
     const authenticationObject = jsonString.find((note) => note.title === "Key")
 
-    if (!authenticationObject) {
+    if (!authenticationObject) { 
         logError("Note not found!!!")
     } else {
         const authentication = authenticationObject.body
