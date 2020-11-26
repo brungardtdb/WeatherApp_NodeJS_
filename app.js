de227194bd79c22bd546ebcdfd587005db01ac91
David Brungardt <brungardtdb@gmail.com>
2020-11-26 09:03:40 -0700

    Added some error handling, broke out on my own a bit with getGPS.js and learned a bit about promises and asynchronous JS.

--- app.js
+++ app.js
@@ -8,27 +8,39 @@ const weatherAuthentication = auth.GetAuthentication(weatherKey)
 // Get latitude and longitude for coor
 const tempCity = "Aurora"
 const tempState = "Illinois"
-const gpsCoordinates = gps.GetCoordinates(tempCity, tempState)
-// const latitude = gpsCoordinates[0]
-// const longitude = gpsCoordinates[1]
 
-console.log(gpsCoordinates)
+const getCoordinates = gps.GetCoordinates(tempCity, tempState).catch(err => console.log(err))
 
-// Create URL for weather app request
-// Broken into several strings should we choose to change anything in url
-// const website = "http://api.weatherstack.com/"
-// const location = latitude + ", " + longitude // lat and long for Aurora Illinois
-// const query = "current?access_key=" + weatherAuthentication + "&query=" + location + "&units=f"
-// const url = website + query
+getCoordinates.then((result) => {
+    console.log(result)
+}).catch(err => console.log(err))
+
+//Create URL for weather app request
+//Broken into several strings should we choose to change anything in url
+const latitude = 41.7606
+const longitude = -88.290001
+const website = "http://api.weatherstack.com/"
+const location = latitude + ", " + longitude // lat and long for Aurora Illinois
+const query = "current?access_key=" + weatherAuthentication + "&query=" + location + "&units=f"
+const url = website + query
 
 // // Request weather data from weatherstack.com
-// request({url: url, json: true}, (error, response) => {
-//     // Parse weather data
-//     const degrees = response.body.current.temperature
-//     const feelsLike = response.body.current.feelslike 
-//     const desc = response.body.current.weather_descriptions[0]
+request({url: url, json: true}, (error, response) => {
+    // Parse weather data
 
-//     const output = desc + ". It is " + degrees + " degrees and it feels like " + feelsLike + " degrees."
-//     console.log(output)
-// })
+    if (error) {
+        console.log(error)
+    } else if (response.body.error) {
+        console.log('Unable to find location')
+    }    
+    else {
+        const degrees = response.body.current.temperature
+        const feelsLike = response.body.current.feelslike 
+        const desc = response.body.current.weather_descriptions[0]
+
+        const output = desc + ". It is " + degrees + " degrees and it feels like " + feelsLike + " degrees."
+        console.log(output)
+    }
+    
+})
 
--- getGPS.js
+++ getGPS.js
@@ -3,24 +3,36 @@ const auth = require('./authentication.js')
 const mapKey = "mapKey"
 const mapAuthentication = auth.GetAuthentication(mapKey)
 
-const GetCoordinates = (city, state) => {
+const GetCoordinates = async (city, state) => {
 
-    console.log(city)
-    console.log(state)
+    const output = []
     // Create URL to fetch GPS coordinates
     const mapUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + city + "%20" + state + ".json?access_token=" + mapAuthentication + "&language=en&limit=1"
 
-    // Request latitude and longitude from location
-    request({url: mapUrl, json: true}, (error, response) => {
-    // Parse map data
-    const latitude = response.body.features[0].center[1]
-    const longitude = response.body.features[0].center[0]
+    return new Promise((resolve, reject) =>{
+        // Request latitude and longitude from location
+        request({url: mapUrl, json: true}, async (error, response) => {
 
-    // Return latitude and longitude
-    const output = []
-    return output
+            if (error) {
+
+                reject(error)
+
+            } else if (response.body.message === "Not Found") {
+                console.log("Location " + response.body.message)
+            }            
+            else {
+                 
+                //Parse map data                 
+                 const latitude = await response.body.features[0].center[1]
+                 const longitude = await response.body.features[0].center[0]
+                 output.push(latitude)
+                 output.push(longitude)
+                 resolve(output);
+            }
+        })
 
     })
+    
 }
 
 module.exports = {
